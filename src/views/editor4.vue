38da642fb1d2ffd1f627d74f397b005f9a94e138
parker <zppro@parkerdeMacBook-Air.local>
2025-05-29 15:10:24 +0800

    操作按钮调整

--- src/views/editor4.vue
+++ src/views/editor4.vue
@@ -19,11 +19,12 @@
       </el-dropdown>
       <!-- <el-button @click="saveArticle" type="danger">暂存文章</el-button> -->
       <el-button @click="handleSaveAppMsg" type="success">暂存</el-button>
+      <el-button @click="handleSyncToWechatDraftBox" type="success">保存到公众号草稿箱</el-button>
       <el-dropdown split-button type="danger">
         其他
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item @click="handleSyncToWechatDraftBox">同步到微信草稿箱</el-dropdown-item>
+            <!-- <el-dropdown-item @click="handleSyncToWechatDraftBox">同步到微信草稿箱</el-dropdown-item> -->
             <el-dropdown-item @click="confirmOpenPublishToWechatDialog">发布到微信</el-dropdown-item>
             <el-dropdown-item divided @click="openSendArticleDialog">同步到其他账号</el-dropdown-item>
           </el-dropdown-menu>
