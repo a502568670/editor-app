b6f2f41af27bbf91a6488945d003a8c93878fce8
parker <zppro@parkerdeMacBook-Air.local>
2025-05-29 14:32:35 +0800

    v0.2.2

--- src/lib/mp_appmsg-tasks.js
+++ src/lib/mp_appmsg-tasks.js
@@ -61,7 +61,7 @@ const publishAppmsg = async ({ cookies, token, send_time,  isFreePublish, list, 
     list = encodeURIComponent(list)
   }
   formdata = `token=${token}&lang=zh_CN&f=json&ajax=1&fingerprint=aa8c8d5bea554e6e4c9ea1ed0dbb354e&random=${Math.random()}&ack=&code=&reprint_info=${reprint_info_json_str}&reprint_confirm=${reprint_confirm}&list=${list}&groupid=&sex=0&country=&province=&city=&send_time=${send_time}&type=10&share_page=1&synctxweibo=0&operation_seq=${operation_seq}&req_id=${req_id}&req_time=${req_time}&sync_version=1&isFreePublish=${isFreePublish}&appmsgid=${appmsgid}&isMulti=${isMulti}&direct_send=1`
-
+  // formdata = `token=${token}&lang=zh_CN&f=json&ajax=1&random=${Math.random()}&ack=&code=&reprint_info=${reprint_info_json_str}&reprint_confirm=${reprint_confirm}&list=${list}&groupid=&sex=0&country=&province=&city=&send_time=${send_time}&type=10&share_page=1&synctxweibo=0&operation_seq=${operation_seq}&req_id=${req_id}&req_time=${req_time}&sync_version=1&isFreePublish=${isFreePublish}&appmsgid=${appmsgid}&isMulti=${isMulti}&direct_send=1`
   verbose_log('api opts:', opts)
   verbose_log('api formdata:', formdata)
   if (send_time > 0) {
