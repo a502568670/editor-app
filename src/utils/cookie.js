0b528f0245dd42804980d1d1f9175fab5d153837
parker <zppro@parkerdeMacBook-Air.local>
2025-06-19 09:28:41 +0800

    fix client cookie encode err

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,7 +1,8 @@
 export const serializeCookie = (arr) => {
   const items = []
   arr.forEach((v) => {
-    items.push(`${encodeURIComponent(v["name"])}=${encodeURIComponent(v["value"])}`)
+    // items.push(`${encodeURIComponent(v["name"])}=${encodeURIComponent(v["value"])}`)
+    items.push(`${encodeURIComponent(v["name"])}=${v["value"]}`)
   });
   // console.log("items=>", items)
   return items.join(";")
