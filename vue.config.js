bba215b659f4683a3478801b7e0e1fa20aead733
502568670@qq.com <502568670@qq.com>
2026-03-04 11:08:31 +0800

    chore: bump version to 0.6.8 and update autoupdate URL

--- vue.config.js
+++ vue.config.js
@@ -54,7 +54,7 @@ module.exports = {
         publish: [
           {
             provider: 'generic',
-            url: 'http://47.96.22.8:8006/autoupdate'
+            url: 'https://static.dajiala.com:9224/static/gaoqingsong/autoupdate'
           }
         ],
         appId: "com.electron.client",  // 保持与 0.5.9 相同，确保可以自动更新
